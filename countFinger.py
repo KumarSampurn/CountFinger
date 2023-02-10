a74e3c507663e00fc0778573cf21fe278c3134e9
KumarSampurn <kumarsampurn@outlook.com>
2023-02-10 11:28:34 +0530

    Added FingerCount

--- countFinger.py
+++ countFinger.py
@@ -2,10 +2,28 @@ import cv2
 import time
 import os
 import handTrackingModule as htm
+import math
 
 ############################
 wCam, hCam = 640, 480
 ############################
+
+
+
+
+def fingerIsopen(fingerArray, flag=1):
+    count=0
+    if(flag==1):
+        if((fingerArray[0][2]>fingerArray[1][2])and 
+            fingerArray[1][2]>fingerArray[2][2] and
+            fingerArray[2][2]>fingerArray[3][2]):
+                count=1
+    else:
+        if(fingerArray[0][1]>fingerArray[1][1]and 
+            fingerArray[1][1]>fingerArray[2][1] and
+            fingerArray[2][1]>fingerArray[3][1]):
+                count=1
+    return count
 
 cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
@@ -35,11 +53,25 @@ while True:
     
     img=detector.findHands(img)
     
+    imgNumber=0
+    #distance between points
+    lmlist= detector.findPosition(img, draw=False)
+    if (len(lmlist)!=0):
+        indexFinger=[lmlist[5],lmlist[6],lmlist[7],lmlist[8]]
+        middleFinger=[lmlist[9],lmlist[10],lmlist[11],lmlist[12]]
+        ringFinger=[lmlist[13],lmlist[14],lmlist[15],lmlist[16]]
+        pinkyFinger=[lmlist[17],lmlist[18],lmlist[19],lmlist[20]]
+        thumb=[lmlist[1],lmlist[2],lmlist[3],lmlist[4]]
+        print(thumb[0][1])
+        
+        count =[fingerIsopen(indexFinger), fingerIsopen(middleFinger),fingerIsopen(ringFinger),
+                fingerIsopen(pinkyFinger),fingerIsopen(thumb,0)]
+        print(count)
+        imgNumber=sum(count)
     
-    h,w,c=overlayList[0].shape
-    img[0:h,0:w]=overlayList[0]
     
-    
+    h,w,c=overlayList[imgNumber].shape
+    img[0:h,0:w]=overlayList[imgNumber]
     
     cTime = time.time()
     fps = 1 / (cTime - pTime)
